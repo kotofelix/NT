8fafdc9f7dbe58a6c942d70d9b679985cf232d7c
Dmitrii I <kotofelix@yandex.ru>
2023-04-28 09:47:15 +0400

    Task7

--- src/main/java/Pack1/Task7.java
+++ src/main/java/Pack1/Task7.java
@@ -1,12 +1,23 @@
 package Pack1;
 
 import javax.lang.model.element.NestingKind;
+import java.util.Scanner;
 
 public class Task7 {
     public static void main(String[] args) {
-        String[] strings;
+        String[] strings = new String[6];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите 6 слов вашего предложения по отдельности: ");
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = scanner.next();
+        }
+        System.out.println("Заполнение массива закончено");
 
-
-
+        for (int i = 0; i < 1; i++) {
+            System.out.print(strings[i].toUpperCase());
+        }
+        for (int i = 1; i < strings.length; i++) {
+            System.out.print(" " + strings[i]);
+        }
     }
 }
---
+++ src/main/java/Pack1/Task8Final.java
@@ -0,0 +1,4 @@
+package Pack1;
+
+public class Task8Final {
+}
